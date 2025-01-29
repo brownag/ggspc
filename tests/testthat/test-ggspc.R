2d60da46df0fe5e3b6f55adea18ae602f6dd9e9f
Andrew Gene Brown <andrew.g.brown@usda.gov>
2025-01-29 09:58:59 -0800

    bump test for soilDB 2.8.7

--- tests/testthat/test-ggspc.R
+++ tests/testthat/test-ggspc.R
@@ -2,7 +2,7 @@ test_that("ggplot dispatch on SoilProfileCollection works", {
   skip_if_not_installed("soilDB")
 
   data(loafercreek, package = "soilDB")
-  aqp::GHL(loafercreek) <- "genhz"
+  aqp::GHL(loafercreek) <- "dspcomplayerid"
   x <- ggplot2::ggplot(loafercreek, ggplot2::aes(earthcovkind1, slope_field)) +
     ggplot2::geom_boxplot(na.rm = TRUE)
   expect_true(inherits(x, 'gg'))
